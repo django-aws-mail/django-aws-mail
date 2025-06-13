0bcb4ca1b6523ca1e3957c14093cc0d0d0430b38
Bas van Gaalen <webtweakers@gmail.com>
2025-06-13 16:47:40 +0200

    Updated HTMLParser to also remove the contents of the title tag

--- django_aws_mail/html.py
+++ django_aws_mail/html.py
@@ -3,22 +3,32 @@ from html.parser import HTMLParser as BaseHTMLParser
 
 
 class HTMLParser(BaseHTMLParser):
+    """
+    Parser that strips all HTML tags from a string,
+    removes the content of the <title> tag,
+    and preserves the URLs in <a> tags.
+    """
     def __init__(self):
         super().__init__(convert_charrefs=True)
+        self._title = False
         self._href = None
         self._text = []
 
     def handle_data(self, data):
-        text = data.strip()
-        if len(text) > 0:
-            text = re.sub('[ \t\r\n]+', ' ', text)
-            self._text.append(text + ' ')
+        if not self._title:
+            text = data.strip()
+            if len(text) > 0:
+                text = re.sub('[ \t\r\n]+', ' ', text)
+                self._text.append(text + ' ')
 
     def handle_starttag(self, tag, attrs):
         if tag == 'p':
             self._text.append('\n\n')
         elif tag == 'br':
             self._text.append('\n')
+        elif tag == 'title':
+            # recording being in title tag, so it's skipped in handle_data
+            self._title = True
         elif tag == 'a':
             # attrs is a list of tuples: [('href', 'link'), ...]
             for attr in attrs:
@@ -29,6 +39,9 @@ class HTMLParser(BaseHTMLParser):
     def handle_endtag(self, tag):
         if tag == 'p':
             self._text.append('\n\n')
+        elif tag == 'title':
+            # end of title tag
+            self._title = False
         elif tag == 'a' and self._href:
             # add the url that was found in starttag
             self._text.append(f'<{self._href}>')
